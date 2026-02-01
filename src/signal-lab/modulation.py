418f5d08d0b2c15c52354f6ee7d844ea21f287bb
maorishakov <maorishakov@gmail.com>
2026-02-01 11:35:22 +0200

    docs: specify QPSK Gray mapping and normalization

---
+++ src/signal-lab/modulation.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+"""
+QPSK Gray mapping:
+
+00 -> (1,1)
+01 -> (-1,1)
+11 -> (-1,-1)
+10 -> (1,-1)
+
+Noramlization - > /sqrt(2)
+"""
+
+def qpsk_modulation(bits: np.ndarray) -> np.ndarray:
+
+    pass
+
